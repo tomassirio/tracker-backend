10fdb788dc0b1bff0de9ed5203c6f0e8c07b1335
tomassirio <tomassirio@gmail.com>
2025-10-02 19:22:04 +0200

    Init Repo

---
+++ src/main/java/com/tomassirio/wanderer/trackerbackend/TrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tomassirio.wanderer.trackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tomassirio/wanderer/trackerbackend/TrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomassirio.wanderer.trackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
