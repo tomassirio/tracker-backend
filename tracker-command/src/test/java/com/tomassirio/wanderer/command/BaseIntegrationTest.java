80c60b9e1292d584eba68988fe68ff56ee820c0c
tomassirio <tomassirio@gmail.com>
2025-10-04 11:35:43 +0200

    Test LocationService

---
+++ tracker-command/src/test/java/com/tomassirio/wanderer/command/BaseIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.tomassirio.wanderer.command;
+
+import com.tomassirio.wanderer.command.config.TestJpaConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@Import(TestJpaConfiguration.class)
+public abstract class BaseIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("tracker_test")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
+
--- tracker-command/src/test/java/com/tomassirio/wanderer/command/TrackerCommandApplicationTests.java
+++ tracker-command/src/test/java/com/tomassirio/wanderer/command/TrackerCommandApplicationTests.java
@@ -1,33 +1,8 @@
 package com.tomassirio.wanderer.command;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import com.tomassirio.wanderer.command.config.TestJpaConfiguration;
 
-@SpringBootTest
-@Testcontainers
-@Import(TestJpaConfiguration.class)
-class TrackerCommandApplicationTests {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
-            .withDatabaseName("tracker_test")
-            .withUsername("test")
-            .withPassword("test");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
-    }
+class TrackerCommandApplicationTests extends BaseIntegrationTest {
 
 	@Test
 	void contextLoads() {
