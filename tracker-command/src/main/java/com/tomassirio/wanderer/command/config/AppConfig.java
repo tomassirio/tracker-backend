bfc44f4b9a3d9166d805e0802e75087bf7a53bff
tomassirio <tomassirio@gmail.com>
2025-10-07 14:54:13 +0200

    Clean up repository

--- tracker-command/src/main/java/com/tomassirio/wanderer/command/config/AppConfig.java
+++ tracker-command/src/main/java/com/tomassirio/wanderer/command/config/AppConfig.java
@@ -1,11 +1,12 @@
 package com.tomassirio.wanderer.command.config;
 
 import com.tomassirio.wanderer.commons.config.CommonsJpaConfiguration;
+import com.tomassirio.wanderer.commons.config.CorsConfig;
 import com.tomassirio.wanderer.commons.config.DatabaseConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 /** Application configuration that imports shared configurations from the commons module. */
 @Configuration
-@Import({DatabaseConfig.class, CommonsJpaConfiguration.class})
+@Import({DatabaseConfig.class, CommonsJpaConfiguration.class, CorsConfig.class})
 public class AppConfig {}
