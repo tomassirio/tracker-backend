ed28ddd2a6e541ca0bee3699934e87eeff5ca198
tomassirio <tomassirio@gmail.com>
2025-10-08 17:24:03 +0200

    Remove CommonsJpaConfiguration.java. Add SecurityConfig

--- commons/src/main/java/com/tomassirio/wanderer/commons/config/CommonsJpaConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.tomassirio.wanderer.commons.config;
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EntityScan("com.tomassirio.wanderer.commons.domain")
-public class CommonsJpaConfiguration {}
--- tracker-command/src/main/java/com/tomassirio/wanderer/command/config/JpaConfiguration.java
+++ tracker-command/src/main/java/com/tomassirio/wanderer/command/config/JpaConfiguration.java
@@ -1,7 +1,7 @@
 package com.tomassirio.wanderer.command.config;
 
-import com.tomassirio.wanderer.commons.config.CommonsJpaConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,4 +13,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @ConditionalOnMissingBean(name = "testJpaConfiguration")
 @EnableJpaRepositories(basePackages = "com.tomassirio.wanderer.command.repository")
-public class JpaConfiguration extends CommonsJpaConfiguration {}
+@EntityScan("com.tomassirio.wanderer.commons.domain")
+public class JpaConfiguration {}
