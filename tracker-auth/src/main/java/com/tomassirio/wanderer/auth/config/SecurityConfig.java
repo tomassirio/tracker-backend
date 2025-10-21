8ce0c0a3c84c1f467eac1aa84cd9543be23c4610
tomassirio <tomassirio@gmail.com>
2025-10-21 16:13:24 +0200

    Add Cors

--- tracker-auth/src/main/java/com/tomassirio/wanderer/auth/config/SecurityConfig.java
+++ tracker-auth/src/main/java/com/tomassirio/wanderer/auth/config/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(cors -> cors.configure(http))
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
